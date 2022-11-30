e92425db6b278b0282caf77ba65e4e6cceb4de3f
juanfco86 <juanfco86@gmail.com>
2022-11-30 18:05:18 +0100

    init proyect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hey
-        </p>
+        <p>Hey</p>
       </header>
     </div>
   );
