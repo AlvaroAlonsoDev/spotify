98a9b7cacfd2c340e983218e92f5e5cc3f0d267d
juanfco86 <juanfco86@gmail.com>
2022-12-08 12:02:02 +0100

    hearth button

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -3,8 +3,9 @@ import './Card.css';
 const Card = ({ data, size, img }) => {
 
     return (
-
-        <div className={size} >
+        
+        <div className={size}>
+            <button className='card__button--like'>❤</button>
             <img className={img} src="https://is1-ssl.mzstatic.com/image/thumb/Features114/v4/9d/3d/dd/9d3ddde2-d671-d9b4-3e21-e872b1529193/U0MtTVMtV1ctRmVlbGluZ19GZXN0aXZlLUFEQU1fSUQ9MTA1NzU5MDMzOC5wbmc.png/632x632cc.webp" alt="Card cap" />
             <div className="card-text card-body">
                 <h5 className="card-title">{data.name}</h5>
